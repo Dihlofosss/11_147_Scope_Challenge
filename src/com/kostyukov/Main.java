00feb705fcbafe4639562987a0e1255d4671ef17
Dihlofosss <maxim.kostyukov@googlemail.com>
2020-10-11 16:22:14 +0200

    2nd solution

--- src/com/kostyukov/Main.java
+++ src/com/kostyukov/Main.java
@@ -4,14 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 	
-	public static Scanner scanner = new Scanner(System.in);
+	public static Scanner x = new Scanner(System.in);
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Please enter the number ");
-		X x = new X(scanner.nextInt());
-		scanner.nextLine();
+		new X(x.nextInt());
+	}
+	
+	public static class X
+	{
+		private int x;
 		
-		x.x();
+		public X(int x)
+		{
+			this.x = x;
+			x();
+		}
+		
+		public void x()
+		{
+			System.out.println("Times table for " + x);
+			for (int x = 1; x <= 12; x++)
+			{
+				System.out.println(this.x + " * " + x + " = " + this.x * x);
+			}
+		}
 	}
 }
